a1342f6bd1ec57c7b77518433c7430e503ef7def
Nguyen Van Hien <nguyenvanhien200895@gmail.com>
2018-11-14 13:10:30 +0700

    Code first

---
+++ src/app/list-name/list-name.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {ListName} from '../listName';
+import { Name } from '../name';
+
+@Component({
+  selector: 'app-list-name',
+  templateUrl: './list-name.component.html',
+  styleUrls: ['./list-name.component.css']
+})
+export class ListNameComponent implements OnInit {
+  arrName = ListName;
+  selectedName: Name;
+
+  constructor() { }
+  ngOnInit() {
+  }
+  
+  onSelect(name: Name): void{
+    this.selectedName = name;
+  }
+}
---
+++ src/app/listName.ts
@@ -0,0 +1,16 @@
+import {Name } from './name';
+
+export const ListName: Name[] =[
+    {id: 11, name: 'Mr. Hien'},
+    {id: 12, name: 'Mr. Hien'},
+    {id: 13, name: 'Mr. Hien'},
+    {id: 14, name: 'Mr. Hien'},
+    {id: 15, name: 'Mr. Hien'},
+    {id: 16, name: 'Mr. Hien'},
+    {id: 17, name: 'Mr. Hien'},
+    {id: 18, name: 'Mr. Hien'},
+    {id: 19, name: 'Mr. Hien'},
+    {id: 20, name: 'Mr. Hien'},
+    {id: 21, name: 'Mr. Hien'},
+    {id: 22, name: 'Mr. Hien'},
+];
